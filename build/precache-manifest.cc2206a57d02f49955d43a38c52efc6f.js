781596c8fb336aa3be9a054b1beee911876b88b0
sadlerb <sadlerb22@gmail.com>
2022-05-21 14:54:59 -0500

    Initial Commit

---
+++ build/precache-manifest.cc2206a57d02f49955d43a38c52efc6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00c836cbd705aeae20ee",
+    "url": "/tinder-clone/static/css/main.d3dc0bbc.chunk.css"
+  },
+  {
+    "revision": "00c836cbd705aeae20ee",
+    "url": "/tinder-clone/static/js/main.f1fcb5cf.chunk.js"
+  },
+  {
+    "revision": "689343a521273927dd38",
+    "url": "/tinder-clone/static/js/runtime~main.a4f65a1a.js"
+  },
+  {
+    "revision": "d1368782f9918f3c9b8a",
+    "url": "/tinder-clone/static/js/2.b76e84df.chunk.js"
+  },
+  {
+    "revision": "1dd546052ab8a0f7d305a91d3b741e0d",
+    "url": "/tinder-clone/index.html"
+  }
+];
